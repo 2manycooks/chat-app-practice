b0b8a9423c2d0975968da68a1976d803dc14c021
Gabriel Gangoso <gabriel.gangoso@gmail.com>
2021-08-25 13:34:58 -0700

    added console log testing for messages

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', (req,res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat mesasge', (msg) => {
+        console.log(msg)
         io.emit('chat message', msg);
     })
     socket.on('disconnect', () => {
